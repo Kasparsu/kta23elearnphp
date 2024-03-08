2987fb87719a7a21b90fa3f4c9a6c0dc926c22b0
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-03-08 11:47:04 +0200

    Magic methods

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 class Box {
     private $weight;
+
+    public function __construct($weight){
+        $this->setWeight($weight);
+        echo "object created \n";
+    }
+
     public function getWeight(){
         return $this->weight;
     }
@@ -12,10 +18,22 @@ class Box {
             throw new Exception('Negative Mass error');
         }
     }
+
+    public function __toString()
+    {
+        return 'Im box and I weigh ' . $this->weight;
+    }
+
+    public function __destruct()
+    {
+        echo "object destroyed \n";
+    }
 }
 
 // sb else code
 
-$box = new Box();
-$box->setWeight(-10);
+echo "before object created \n";
+$box = new Box(10);
 var_dump($box);
+echo "after object created \n";
+echo $box;
