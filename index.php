3f9d9b6ddb556e8b7facc3546dcfd705f4c60df9
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-03-08 09:06:59 +0200

    Class example

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
-echo 'Hello World!' . time();
+class Box {
+    public $width;
+    public $heigth;
+    public $length;
+    public $material;
+
+    public function describe(){
+        echo 'width: ' . $this->width . ' height: ' . $this->heigth . ' lenght: ' . $this->length;
+    }
+}
+
+$box1 = new Box();
+$box1->width = 1;
+$box1->heigth = 2;
+$box1->length = 3;
+$box1->material = 'Wood';
+var_dump($box1);
+
+$box2 = new Box();
+$box2->width = 5;
+$box2->heigth = 6;
+$box2->length = 7;
+$box2->material = 'Metal';
+var_dump($box2);
+var_dump($box1);
