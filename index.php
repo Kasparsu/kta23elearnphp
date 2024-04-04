0765f4954162d8c7de01de0d9ac482008c37640c
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-04-04 12:57:58 +0300

    php templating

--- index.php
+++ index.php
@@ -1,39 +1,33 @@
 <?php
+$name = 'Kaspar';
+$fruits = ['Apple', 'Banana', 'Cherry', 'Pear'];
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-class Box {
-    private $weight;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-    public function __construct($weight){
-        $this->setWeight($weight);
-        echo "object created \n";
-    }
+<body>
+    <h1>Hello <?=$name?></h1>
+    <ul>
+        <?php foreach ($fruits as $fruit) : ?>
+            <?php if ($fruit != 'Apple') : ?>
+                <li>
+                    <?=$fruit?>
+                </li>
+            <?php else : ?>
+                <li>
+                    <i>
+                        <?=$fruit?>
+                    </i>
+                </li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
+</body>
 
-    public function getWeight(){
-        return $this->weight;
-    }
-    public function setWeight($weight){
-        if($weight > 0){
-            $this->weight = $weight;
-        } else {
-            throw new Exception('Negative Mass error');
-        }
-    }
-
-    public function __toString()
-    {
-        return 'Im box and I weigh ' . $this->weight;
-    }
-
-    public function __destruct()
-    {
-        echo "object destroyed \n";
-    }
-}
-
-// sb else code
-
-echo "before object created \n";
-$box = new Box(10);
-var_dump($box);
-echo "after object created \n";
-echo $box;
+</html>
